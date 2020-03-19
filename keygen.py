496b0b44abc77d42824fe4db0cd74373c38e81a5
Paushali Kundu <paushali.kundu@thomsonreuters.com>
2020-03-19 17:25:41 -0500

    another bug

--- keygen.py
+++ keygen.py
@@ -1,5 +1,6 @@
 from Crypto.PublicKey import RSA
 import boto3
+import json
 
 KEY = RSA.generate(2048)
 
@@ -8,7 +9,7 @@ def gen_privatekey():
     private_key = KEY.export_key()
     client = boto3.client('secretsmanager')
     response = client.put_secret_value(SecretId='arn:aws:secretsmanager:us-east-1:486076294107:secret:a205257/cfsigner/source-secret-C5X6dN',
-                                       SecretString=private_key)
+                                       SecretString=json.dumps(private_key))
     return response
 
 
