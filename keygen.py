4f7484b6553f05c42dbbdd75765c4b1e2480afc1
Paushali Kundu <paushali.kundu@thomsonreuters.com>
2020-03-19 17:15:28 -0500

    put private key to secret

--- keygen.py
+++ keygen.py
@@ -1,23 +1,15 @@
 from Crypto.PublicKey import RSA
-# import boto3
+import boto3
 
 KEY = RSA.generate(2048)
 
 
-def get_privatekey():
+def gen_privatekey():
     private_key = KEY.export_key()
-    # file_out = open("private.pem", "wb")
-    # file_out.write(private_key)
-    # private_key = private_key.decode("utf-8")
-    print(type(private_key))
-    return private_key
-
-
-# def put_privatekey(private_key):
-#     client = boto3.client('secretsmanager')
-#     response = client.put_secret_value(SecretId='arn:aws:secretsmanager:us-east-1:486076294107:secret:a205257/cfsigner/source-secret-C5X6dN',
-#                                        SecretString=private_key)
-#     return response
+    client = boto3.client('secretsmanager')
+    response = client.put_secret_value(SecretId='arn:aws:secretsmanager:us-east-1:486076294107:secret:a205257/cfsigner/source-secret-C5X6dN',
+                                       SecretString=private_key)
+    return response
 
 
 def get_publickey():
@@ -32,3 +24,4 @@ def get_publickey():
 if __name__ == '__main__':
     public_key = get_publickey()
     print(public_key)
+    print(get_publickey)
